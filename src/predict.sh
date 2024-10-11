960e03e26439fed3d14da40999feb83441cf295b
ipitio <21136719+ipitio@users.noreply.github.com>
2024-10-11 17:09:13 -0400

    mv cmds into script

--- src/predict.sh
+++ src/predict.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-# shellcheck disable=SC1091
+# shellcheck disable=SC1091,SC2015
 
 set -Eeuo pipefail
 declare -r pdf_dir="${1:-.}"
 export OMP_THREAD_LIMIT=1
+
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip python3-venv tesseract-ocr poppler-utils
 python3 -m venv venv
 
 if [[ -f venv/bin/pip3 ]]; then
     source venv/bin/activate
-
-    ./venv/bin/pip3 install -r requirements.txt
+    ./venv/bin/pip3 install --no-cache-dir -r requirements.txt
     ./venv/bin/python3 ./main.py "$pdf_dir"
-
     deactivate
-    [[ "$pdf_dir" == "." ]] || rm -rf venv
 elif [[ -f /.dockerenv ]]; then
     export PATH="$PATH:/root/.local/bin"
     pip3 install -r requirements.txt --user --break-system-packages
