4f48cc35e9993f9c2de8011ca092ff2e2ae4e1e1
ipitio <21136719+ipitio@users.noreply.github.com>
2024-10-11 21:20:01 -0400

    find

--- src/predict.sh
+++ src/predict.sh
@@ -10,18 +10,16 @@ for _ in {1..300}; do
     apt-get install -y python3 python3-pip python3-venv tesseract-ocr poppler-utils && break || sleep 1
 done
 
-if [ -n "$1" ]; then
-    export OMP_THREAD_LIMIT=1
-    python3 -m venv venv
+export OMP_THREAD_LIMIT=1
+[ -d venv ] || python3 -m venv venv
 
-    if [[ -f venv/bin/pip3 ]]; then
-        source venv/bin/activate
-        find . -name requirements.txt -exec ./venv/bin/pip3 install --no-cache-dir -r {} \;
-        ./venv/bin/python3 ./main.py "$1"
-        deactivate
-    elif [[ -f /.dockerenv ]]; then
-        export PATH="$PATH:/root/.local/bin"
-        pip3 install -r requirements.txt --user --break-system-packages
-        python3 ./main.py "$1"
-    fi
+if [[ -f venv/bin/pip3 ]]; then
+    source venv/bin/activate
+    find . -name requirements.txt -exec ./venv/bin/pip3 install --no-cache-dir -r {} \;
+    [ -z "$1" ] || find . -name main.py -exec ./venv/bin/python3 {} "$1" \;
+    deactivate
+elif [[ -f /.dockerenv ]]; then
+    export PATH="$PATH:/root/.local/bin"
+    pip3 install -r requirements.txt --user --break-system-packages
+    [ -z "$1" ] || python3 ./main.py "$1"
 fi
