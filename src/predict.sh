f012dca39083ec8c583000a928c937919b63973f
ipitio <21136719+ipitio@users.noreply.github.com>
2024-10-11 20:34:03 -0400

    use 1

--- src/predict.sh
+++ src/predict.sh
@@ -10,18 +10,17 @@ for _ in {1..300}; do
     apt-get install -y python3 python3-pip python3-venv tesseract-ocr poppler-utils && break || sleep 1
 done
 
-set -Eeuo pipefail
-declare -r pdf_dir="${1:-.}"
+[ -n "$1" ] || exit
 export OMP_THREAD_LIMIT=1
 python3 -m venv venv
 
 if [[ -f venv/bin/pip3 ]]; then
     source venv/bin/activate
     find . -name requirements.txt -exec ./venv/bin/pip3 install --no-cache-dir -r {} \;
-    ./venv/bin/python3 ./main.py "$pdf_dir"
+    ./venv/bin/python3 ./main.py "$1"
     deactivate
 elif [[ -f /.dockerenv ]]; then
     export PATH="$PATH:/root/.local/bin"
     pip3 install -r requirements.txt --user --break-system-packages
-    python3 ./main.py "$pdf_dir"
+    python3 ./main.py "$1"
 fi
